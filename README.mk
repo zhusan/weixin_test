847f61aded5082e15cf69f6500ea6cefa36efa4a
zhusan <zhusan333@163.com>
2014-03-28 16:51:00 +0800

    Update and rename README.rdoc to README.mk

---
+++ README.mk
@@ -0,0 +1,16 @@
+weixin公众号接入测试
+==============
+
+此demo使用了
+[weixin_rails_middleware](https://github.com/lanrion/weixin_rails_middleware) 
+和
+[weixin_authorize](https://github.com/lanrion/weixin_authorize)
+这2个gem进行测试
+
+线上地址：http://zhusan.ap01.aws.af.cm/
+
+公众号管理地址：http://zhusan.ap01.aws.af.cm/public_account
+
+菜单管理地址:http://zhusan.ap01.aws.af.cm/diymenus  （scaffold界面）
+
+推送菜单到微信公众号：http://zhusan.ap01.aws.af.cm/diymenus/generate_menu
